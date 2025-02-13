f74d58305b003bdb291678d80cc8cb064c9edf87
Rémy Guillermin <remy.guillermin.73@gmail.com>
2025-02-13 16:43:28 +0100

    Update save_plot

--- modules/croco_plot/utils.py
+++ modules/croco_plot/utils.py
@@ -158,7 +158,12 @@ def save_figure(fig, filename):
     output_dir = '/lus/home/CT1/c1601279/rguillermin/IGE-Stochastic/figures'
     os.makedirs(output_dir, exist_ok=True)
     fig.savefig(os.path.join(output_dir, filename))
-    print(f"Figure saved as {filename}.")
+    print(f
+"""
+Figure saved as {filename}.
+To open the figure, run: cplot.utils.open_figure('{filename}')
+"""
+)
 
 
 def open_figure(filenames):
