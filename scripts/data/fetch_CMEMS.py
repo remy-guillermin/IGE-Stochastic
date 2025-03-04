88fbd5009ae01d26254b9536c565d1e8028528dd
Rémy Guillermin <remy.guillermin.73@gmail.com>
2025-03-04 14:27:06 +0100

    Convert to python script

---
+++ scripts/data/fetch_CMEMS.py
@@ -0,0 +1,16 @@
+import copernicusmarine
+#copernicusmarine.login() # Nécessaire pour se logger sur le serveur de Copernicus Marine
+from pprint import pprint
+
+output_directory = 'home/guilremy/IGE-Stochastic/data/RAW/'
+
+get_result = copernicusmarine.get(
+    dataset_id='METOFFICE-GLO-SST-L4-REP-OBS-SST', # ID du dataset sur https://data.marine.copernicus.eu/products
+    dry_run=False, # Si True, affiche les fichiers à télécharger sans les télécharger
+    filter="*2018/*", # Filtre les fichiers à télécharger
+    output_directory=output_directory, # Répertoire de sortie
+    #create_file_list= output_directory + "files_to_download.txt", # Crée un fichier avec la liste des fichiers à télécharger
+    no_directories=True, # Si True, télécharge les fichiers dans le répertoire de sortie sans créer de sous-répertoires
+)
+
+pprint(f"List of saved files: {get_result}")
